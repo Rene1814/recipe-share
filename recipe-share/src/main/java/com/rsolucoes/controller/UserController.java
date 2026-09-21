d0e8fd1fb04dfd6d0a6d10e873e66650b066450d
Renê Souza <rne_souza@yahoo.com.br>
2026-09-20 23:09:41 -0300

    UserRepsitory created

--- recipe-share/src/main/java/com/rsolucoes/controller/UserController.java
+++ recipe-share/src/main/java/com/rsolucoes/controller/UserController.java
@@ -5,13 +5,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsolucoes.model.User;
+import com.rsolucoes.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
+@AllArgsConstructor
 public class UserController {
+	
+	private UserRepository userRepository;
 
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
+	public User createUser(@RequestBody User user) throws Exception{
 		
-		return user;
+		User isExist = userRepository.findByEmail(user.getEmail());
+		if (isExist != null) {
+			throw new Exception("user is exist whith email " + user.getEmail());
+		}
+		
+		User savedUser = userRepository.save(user);
+		
+		return savedUser;
 	}
+	
+//	public User findByEmail(String email) throws Exception{
+//		User user = userRepository.findByEmail(email);
+//		if (user == null) {
+//			throw new Exception("user not found whith email " + email);
+//		}
+//		return user;
+//	}
 }
--- recipe-share/src/main/java/com/rsolucoes/model/User.java
+++ recipe-share/src/main/java/com/rsolucoes/model/User.java
@@ -1,5 +1,9 @@
 package com.rsolucoes.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +13,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	private String password;
 	private String email;
 	private String fullName;
